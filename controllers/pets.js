f9492729c191b5e1cd79e36815298411e54d72eb
abdulrahman-alhattab <osmiumh30@gmail.com>
2025-12-28 15:55:13 +0300

    finish create pet

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,13 +1,20 @@
 //express
-const express = require('express');
+const express = require('express')
 //model
-const Pet = require('../model/pet');
+const Pet = require('../model/pet')
 
 //express router
-const router = express.Router();
+const router = express.Router()
 //post + /pets/
-router.post('/', async (req,res) => {
-res.json(req.body)
+router.post('/', async (req, res) => {
+  try {
+    const pet = await Pet.create(req.body)
+
+    res.status(201).json({ pet })
+  } catch (err) {
+    res.status(500).json({ error: 'failed to create pet' })
+    console.error(err)
+  }
 })
 //export router
 module.exports = router
--- server.js
+++ server.js
@@ -2,20 +2,22 @@
 
 const dotenv = require('dotenv')
 dotenv.config()
+
 //mongoose
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on('connected', () => {
   console.log('connected to DB')
 })
+
 //express
 const express = require('express')
 const app = express()
 //controllers
 const morgan = require('morgan')
 const petCtrl = require('./controllers/pets')
-//middleWare
 
+//middleWare
 app.use(morgan('dev'))
 app.use(express.json())
 
